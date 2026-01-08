f244fc3c2116fecbca77a6691dc5d49921c0e258
Vasya <br2876@bs.amalnet.k12.il>
2026-01-08 09:33:06 +0200

    marriage

--- app/src/main/java/com/example/the_best_fun_vecation/MainActivity.java
+++ app/src/main/java/com/example/the_best_fun_vecation/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.the_best_fun_vecation;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button type;
+    String t;
+
+    AlertDialog.Builder dtype;
+    Button gear;
+    String g;
+
+    AlertDialog.Builder dgear;
+    Button name;
+    String n;
+
+    AlertDialog.Builder dname;
+    Button sure;
+    String s;
+    AlertDialog.Builder dsure;
+
+
+
+    public void marriage(){
+        type = findViewById(R.id.type);
+        gear = findViewById(R.id.gear);
+        name = findViewById(R.id.name);
+        sure = findViewById(R.id.sure);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        marriage();
     }
 }
