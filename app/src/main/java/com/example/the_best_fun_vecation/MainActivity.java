00319f87a23b856c042b3d6ce7d3fab2dd77e841
Vasya <br2876@bs.amalnet.k12.il>
2026-01-08 15:48:20 +0200

    all the buttons work

--- app/src/main/java/com/example/the_best_fun_vecation/MainActivity.java
+++ app/src/main/java/com/example/the_best_fun_vecation/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txt;
 
     Button type;
-    String t;
+    String t = "";
 
     AlertDialog.Builder dtype;
     Button gear;
@@ -23,15 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
     AlertDialog.Builder dgear;
     Button name;
-    String n;
+    String n = "";
 
     AlertDialog.Builder dname;
     Button sure;
     AlertDialog.Builder dsure;
 
 
-
-    public void marriage(){
+    public void marriage() {
         type = findViewById(R.id.type);
         gear = findViewById(R.id.gear);
         name = findViewById(R.id.name);
@@ -50,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void What_type(View view) {
         final String[] fun_activities = {"museum", "ballet", "restaurant", "pub"};
-
         dtype = new AlertDialog.Builder(this);
         dtype.setTitle("type of vacation");
+        dtype.setCancelable(false);
 
 
         dtype.setItems(fun_activities, new DialogInterface.OnClickListener() {
@@ -67,10 +67,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void What_gear(View view) {
-        final String[] fun_activities = {"museum", "ballet", "restaurant", "pub"};
+        final String[] fun_activities = {"drinks", "pillow", "snack", "fancy cloths"};
 
         dgear = new AlertDialog.Builder(this);
         dgear.setTitle("gear");
+        dgear.setCancelable(false);
         dgear.setPositiveButton("ok", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -88,8 +89,8 @@ public class MainActivity extends AppCompatActivity {
         dgear.setMultiChoiceItems(fun_activities, null, new DialogInterface.OnMultiChoiceClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which, boolean isChecked) {
-                if ((isChecked) && (!g.contains(fun_activities[which])) )
-                    g = g + " " + fun_activities[which];
+                if ((isChecked) && (!g.contains(fun_activities[which])))
+                    g = g +fun_activities[which] + " ";
                 else {
                     g = g.replace(fun_activities[which], "");
                 }
@@ -104,17 +105,55 @@ public class MainActivity extends AppCompatActivity {
     public void What_name(View view) {
         dname = new AlertDialog.Builder(this);
         dname.setTitle("name");
+        dname.setCancelable(false);
+        final EditText et = new EditText(this);
+        et.setHint("type your name please");
+        dname.setView(et);
+
+        dname.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                n = et.getText().toString();
+            }
+        });
+
+        dname.setNegativeButton("cansel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
 
         dname.show();
     }
 
+
+
     public void Are_sure(View view) {
         dsure = new AlertDialog.Builder(this);
         dsure.setTitle("sure");
+        dsure.setMessage("are you sure you want me to generate the vacation?");
+        dsure.setCancelable(false);
+        dsure.setPositiveButton("sure", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                //txt.setText(t + "\n" + g + "\n" + n);
+                txt.setText("you will go to the " + t + "\n" +
+                         "you will take with you " + g + "\n" +
+                        "congratulations " +n);
+                dialog.dismiss();
+            }
+        });
+
+        dsure.setNegativeButton("no no no", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which){
+                dialog.cancel();
+            }
+        });
 
         dsure.show();
 
-        txt.setText(t + "\n" + g + "\n" + n);
 
 
     }
