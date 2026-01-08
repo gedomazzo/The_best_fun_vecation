eccb1318bc7def530c13117a9f003284b2122b16
Vasya <br2876@bs.amalnet.k12.il>
2026-01-08 10:23:08 +0200

    undestanding how to make choices

--- app/src/main/java/com/example/the_best_fun_vecation/MainActivity.java
+++ app/src/main/java/com/example/the_best_fun_vecation/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.the_best_fun_vecation;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String[] colors = {"red", "green", "blue"};
 
     Button type;
     String t;
@@ -40,4 +45,48 @@ public class MainActivity extends AppCompatActivity {
 
         marriage();
     }
+
+
+    public void What_type(View view) {
+        int [] color = new int[]{0, 0, 0};
+
+        dtype = new AlertDialog.Builder(this);
+        dtype.setTitle("type of vacation");
+
+
+        dtype.setItems(colors, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                color[which] = 255;
+                getWindow().getDecorView().setBackgroundColor(Color.rgb(color[0], color[1], color[2]));
+            }
+        });
+
+
+        dtype.show();
+    }
+
+    public void What_gear(View view) {
+        dgear = new AlertDialog.Builder(this);
+        dgear.setTitle("gear");
+
+        dgear.show();
+    }
+
+
+    public void What_name(View view) {
+        dname = new AlertDialog.Builder(this);
+        dname.setTitle("name");
+
+        dname.show();
+    }
+
+    public void Are_sure(View view) {
+        dsure = new AlertDialog.Builder(this);
+        dsure.setTitle("sure");
+
+        dsure.show();
+    }
+
+
 }
